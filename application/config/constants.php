5f8d80892dcdf3e13722b71510ed279376fbf83f
dev1 <dev@octopuscart.com>
2018-09-15 12:06:07 +0530

    changes

--- application/config/constants.php
+++ application/config/constants.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $baselink = 'http://'.$_SERVER['SERVER_NAME'];
 
-$baselinkmain = strpos($baselink, '192.168') ? $baselink.'/classApartStoreAdmin/' : 'http://manage.classapartstore.com/';
+$baselinkmain = strpos($baselink, '192.168') ? $baselink.'/octopusadmin/' : 'http://admin.octopuscart.com/';
 define('imageserver', $baselinkmain."assets_main/productimages/");
 
 define('imageserverslider', $baselinkmain."assets_main/sliderimages/");
