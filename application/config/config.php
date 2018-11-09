e27bc6cd3e9f3d2cb0fa638ac8834fb3d752d9e6
dev1 <dev@octopuscart.com>
2018-11-09 15:00:08 +0530

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -390,13 +390,38 @@ $config['encryption_key'] = '';
   | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
   |
  */
-$config['sess_driver'] = 'files';
+
+$baselink = 'http://' . $_SERVER['SERVER_NAME'];
+switch ($baselink) {
+    case "http://localhost":
+       $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+        break;
+    case "http://192.168.1.2":
+        $config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+        break;
+    default:
+        $config['sess_cookie_name'] = 'ci_session';
+        $config['sess_expiration'] = 7200;
+        $config['sess_driver'] = 'database';
+        $config['sess_save_path'] = 'ci_sessions';
+        $config['sess_match_ip'] = FALSE;
+        $config['sess_time_to_update'] = 300;
+        $config['sess_regenerate_destroy'] = FALSE;
+}
+
+
 
 /*
   |--------------------------------------------------------------------------
